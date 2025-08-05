6cd6524b3efa8d442672a153830151b10ff2ab45
lalicanpshs <b2029btdlalican@pshs.edu.ph>
2025-08-05 08:48:59 +0800

    1st nongraded

---
+++ public/script.js
@@ -0,0 +1,6 @@
+let age = 14;
+let maxAge = 100;
+let numPerDay = 2;
+let totalRequired = (numPerDay * 365) * (maxAge - age);
+
+document.write("You will need " + totalRequired + " cups of water needed to last you until the ripe old age of " + maxAge);
