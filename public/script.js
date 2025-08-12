191907311ebceba5834c91d0dc3b71f48f66a9df
lalicanpshs <b2029btdlalican@pshs.edu.ph>
2025-08-12 08:22:17 +0800

    Fifth Commit

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
-let age = 14;
-let maxAge = 100;
-let numPerDay = 2;
+let age = window.prompt("Enter your age.");
+let maxAge = window.prompt("Enter the max age you would like.");
+let numPerDay = window.prompt("Enter how much of your favorite snack you consume in a day.");
 let totalRequired = (numPerDay * 365) * (maxAge - age);
 
-document.write("You will need " + totalRequired + " cups of water needed to last you until the ripe old age of " + maxAge);
+    if (confirm(`Are ${age}, ${maxAge}, and ${numPerDay} truly your inputs?`)) {
+        document.write("You will need " + totalRequired + " cups of water needed to last you until the ripe old age of " + maxAge);
+    }
+    else {
+        window.alert("Run the code again.");
+    }
